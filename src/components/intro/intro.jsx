2ce4de978bc08e99185990095c947741339475c0
Aryan Kumar <128920734+09aryan@users.noreply.github.com>
2023-07-09 22:15:08 +0530

    update

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 import './intro.css'
+import Github from '../../img/github.png'
+import instagram from '../../img/instagram.png'
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+
+import linkedin from '../../img/linkedin.png'
+
 export class Intro extends Component {
   render() {
     return (
@@ -11,9 +22,21 @@ export class Intro extends Component {
             <span>FffffffffffffffffffffffffffffffffffffffffffffffffffffffffffFffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</span>
             </div>   
   <button className='button i-button'>Hire me</button>
+  <div className="i-icons">
+    <a href='/'>
+    <img src={Github} alt=""/>
+    </a>
+   
+    <img src={instagram} alt=""/>
+    <img src={linkedin} alt=""/>
+
+  </div>
         </div>
         <div className="i-right">
-            I am righ side
+        <img src={Vector1} alt=""/>
+    <img src={Vector2} alt=""/>
+    <img src={boy} alt=""/>
+    
         </div>
       </div>
     )
